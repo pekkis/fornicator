8ef60b397f63920c565f513d806d78f760b61378
Mikko Forsström <puhemies@diktaattoriporssi.com>
2013-05-29 15:48:16 +0300

    Fixores majores.

--- web/fornicator.php
+++ web/fornicator.php
@@ -1,18 +1,31 @@
 <?php
 
-if (isset($_GET['apc'])) {
-    apc_clear_cache();
-    apc_clear_cache("user");
-    echo "cleared apc cache...";
-}
-
 if (isset($_GET['stat'])) {
     clearstatcache();
     clearstatcache(true);
-    echo "cleared realpath cache...";
+    echo "Cleared realpath cache...";
 }
 
+// APC fornicator
+if (isset($_GET['apc'])) {
+
+    if (extension_loaded('apc')) {
+        apc_clear_cache();
+        apc_clear_cache("user");
+        echo "cleared apc cache...";
+    }
+}
+
+
 if (isset($_GET['optimizer'])) {
-    accelerator_reset();
-    echo "cleared optimizer cache...";
+
+    if (extension_loaded("ZendOpCache")) {
+        opcache_reset();
+        echo "cleared optimizer cache (ZendOpCache)...";
+    }
+
+    if (extension_loaded("ZendOptimizerPlus")) {
+        accelerator_reset();
+        echo "cleared optimizer cache (ZendOptimizerPlus)...";
+    }
 }
